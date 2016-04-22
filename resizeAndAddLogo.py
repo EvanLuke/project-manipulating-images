2fb49610922510497306ebc0282499932c3a6019
Evan Luke <eluke1@mail.valenciacollege.edu>
2016-04-22 16:37:07 -0400

    10

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -14,7 +14,7 @@ logoWidth, logoHeight = logoIm.size
 #TODO: Save changes.
 
 os.makedirs('withLogo', exist_ok=True)
-
+os.chdir(os.path.join('./originals')
 for filename in os.listdir('originals'):
     if not (filename.endswith('.png') or filename.endswith('.jpg')) \
     or filename == LOGO_FILENAME:
